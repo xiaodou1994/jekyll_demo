5977f94041f58976739dd591f9c5fdf775b5faa1
熊珍珍 <1084509216@qq.com>
2024-05-21 17:09:42 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">Home1</RouterLink>
+        <RouterLink to="/about">About1</RouterLink>
       </nav>
     </div>
   </header>
